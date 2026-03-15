5a2e166b6a3f1907816d8f18579d0a686738338c
petoz <peterzapletal@gmail.com>
2026-03-15 20:56:04 +0100

    first working prototype - level 1-11

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,14 @@ const char* password = "YOUR_PASS";
 
 // ESP32 -> CD74HCT4067
 // Seed Studio ESP32C6 Dev Board
-const int PIN_S0 = 0; // D0
+const int PIN_S0 = 23; // D5
 const int PIN_S1 = 1; // D1
 const int PIN_S2 = 2; // D2
 const int PIN_S3 = 21; // D3 - S3 pin je na ESP32 pripojený cez 10k pull-up rezistor, takže ho môžeme použiť aj ako výstup
 const int PIN_EN = 22; // D4  // active LOW
 
 // dĺžka pulzu na zvolenie stupňa
-const unsigned long PULSE_MS = 10000;
+const unsigned long PULSE_MS = 200;
 
 // čas medzi zmenami stupňa
 const unsigned long STAGE_HOLD_MS = 10000;
@@ -48,7 +48,19 @@ void pulseStage(int stage) {
   muxDisable();
 }
 
+void wifi_ant() {
+  pinMode(WIFI_ENABLE, OUTPUT); // pinMode(3, OUTPUT);
+  digitalWrite(WIFI_ENABLE, LOW); // digitalWrite(3, LOW); // Activate RF switch control
+
+  delay(100);
+
+  pinMode(WIFI_ANT_CONFIG, OUTPUT); // pinMode(14, OUTPUT);
+  digitalWrite(WIFI_ANT_CONFIG, LOW); // digitalWrite(14, HIGH); // Use external antenna
+}
+
+
 void connectWifi() {
+  wifi_ant();
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -65,6 +77,10 @@ void connectWifi() {
 
 void setup() {
   Serial.begin(115200);
+  delay(1000);
+  Serial.println("Starting up...");
+  connectWifi();
+  Serial.println("WiFi connected.");
 
   pinMode(PIN_S0, OUTPUT);
   pinMode(PIN_S1, OUTPUT);
@@ -75,7 +91,6 @@ void setup() {
   muxDisable();
   setMuxChannel(0);
 
-  connectWifi();
 
   Serial.println("Init stage 0...");
   pulseStage(0);
